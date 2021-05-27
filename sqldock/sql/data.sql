5398aa34d97970034f475d4789fb4bc0564c8149
Kunal Kumbhar <60615909+mrfamouskk7@users.noreply.github.com>
2021-05-27 10:27:56 +0530

    Update data.sql

--- sqldock/sql/data.sql
+++ sqldock/sql/data.sql
@@ -6,3 +6,4 @@ mobile int,
 pan varchar(10),
 PRIMARY KEY(pan)
 );
+INSERT INTO internData VALUES ("Kunal1","Kolhapur",1212123414,ALWBS50127),("Kunal2","Kolhapur",2212123414,BLWBS50127),("Kunal3","Kolhapur",3212123414,CLWBS50127);
