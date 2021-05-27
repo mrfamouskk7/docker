1e2cd7ca0118a736418b5224d6e0134ce7a105c4
Kunal Kumbhar <60615909+mrfamouskk7@users.noreply.github.com>
2021-05-27 11:06:37 +0530

    Update data.sql

--- sqldock/sql/data.sql
+++ sqldock/sql/data.sql
@@ -3,7 +3,7 @@ CREATE TABLE `internData` (
   `Name` varchar(50) NOT NULL,
   `Address` varchar(300) NOT NULL,
   `Mobile_Number` int NOT NULL,
-  `PAN_Number` int NOT NULL
+  `PAN_Number` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 LOCK TABLES `internData` WRITE;
-INSERT INTO `internData` VALUES ('Abhishek','Pune',12345,3231),('Nishant','Mumbai',231332,32312);
+INSERT INTO `internData` VALUES ('Kunal','Kolhapur',12345,'BLWAH1280W'),('Nishant','Nashik',231332,'CLWAH1280W'),('Rishi','Patna',231332,'DLWAH1280W');
