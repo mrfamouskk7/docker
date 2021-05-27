54f599781c4275c9dda5ed5b806998911000531d
Kunal Kumbhar <60615909+mrfamouskk7@users.noreply.github.com>
2021-05-27 10:27:45 +0530

    Delete insert.sql

--- sqldock/sql/insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO internData(name,address,mobile,pan) VALUES ("Kunal1","Kolhapur",1212123414,ALWBS50127);
-INSERT INTO internData(name,address,mobile,pan) VALUES ("Kunal2","Kolhapur",2212123414,BLWBS50127);
-INSERT INTO internData(name,address,mobile,pan) VALUES ("Kunal3","Kolhapur",3212123414,CLWBS50127);
-INSERT INTO internData(name,address,mobile,pan) VALUES ("Kunal4","Kolhapur",4212123414,DLWBS50127);
