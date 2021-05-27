36c771d5d75e6bcac2b2ccbb5b468d460a7351c8
Kunal Kumbhar <60615909+mrfamouskk7@users.noreply.github.com>
2021-05-27 11:04:33 +0530

    Update data.sql

--- sqldock/sql/data.sql
+++ sqldock/sql/data.sql
@@ -1,9 +1,9 @@
-CREATE TABLE internData
-(
-name varchar(20),
-address varchar(50),
-mobile int,
-pan varchar(10),
-PRIMARY KEY(pan)
-);
-INSERT INTO internData VALUES ("Kunal1","Kolhapur",1212123414,ALWBS50127),("Kunal2","Kolhapur",2212123414,BLWBS50127),("Kunal3","Kolhapur",3212123414,CLWBS50127);
+DROP TABLE IF EXISTS `internData`;
+CREATE TABLE `internData` (
+  `Name` varchar(50) NOT NULL,
+  `Address` varchar(300) NOT NULL,
+  `Mobile_Number` int NOT NULL,
+  `PAN_Number` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+LOCK TABLES `internData` WRITE;
+INSERT INTO `internData` VALUES ('Abhishek','Pune',12345,3231),('Nishant','Mumbai',231332,32312);
